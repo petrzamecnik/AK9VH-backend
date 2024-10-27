8e907f6c57f17cd80fab9cdf52de756575924493
Petr Zamecnik <Petazamecek@gmail.com>
2024-10-27 11:04:01 +0100

    updated imports

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { connectDB } from './config/database';
+import { connectDB } from '@config/database';
 import userRoutes from './routes/userRoutes';
 
 dotenv.config();
